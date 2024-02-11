49f7ec8f28089cb64da542920c1b25ab7c2662f8
Baldomar <jeisonventy10@gmail.com>
2024-02-11 12:02:15 -0600

    Finalizado

--- Lab2 - Computo 2/Scripts.js
+++ Lab2 - Computo 2/Scripts.js
@@ -1,17 +1,17 @@
 // Mouseover
 document.getElementById('box1').addEventListener('mouseover', function() {
-    this.style.backgroundColor = 'lightgreen';
+    this.style.backgroundColor = 'green';
     document.getElementById('description1').innerText = "Es una función en la que un usuario mueve el cursor sobre un elemento y produce una respuesta.";
 });
 
 // Click
 document.getElementById('box2').addEventListener('click', function() {
-    this.style.backgroundColor = 'lightcoral';
+    this.style.backgroundColor = 'red';
     document.getElementById('description2').innerText = "Es una función en la que un usuario hace clic en él y produce una respuesta.";
 });
 
 // Focus
 document.getElementById('box3').addEventListener('focus', function() {
-    this.style.backgroundColor = 'lightyellow';
+    this.style.backgroundColor = 'black';
     document.getElementById('description3').innerText = "Este cuadro cambia de color cuando recibes el enfoque.";
 });
