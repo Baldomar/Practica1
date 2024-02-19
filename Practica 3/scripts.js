014a879389fde2b5589f38e9738886019f516a2c
Baldomar <jeisonventy10@gmail.com>
2024-02-19 07:11:13 -0600

    Listo

--- Practica 3/scripts.js
+++ Practica 3/scripts.js
@@ -1,22 +1,9 @@
-function funcion1(){
-    num1 = (document.getElementById("num1")).value;
-    num2 = (document.getElementById("num2")).value;
-    //document.write(+sumar(num1,num2));
-    var suma = sumar(num1,num2)
-    resultado = (document.getElementById("resultado"))
-    resultado.innerHTML= suma;
+//Arreglo vacio 
+var vacio = [];
 
-
-}
-
-function sumar(n1,n2){
-    var num1 =Number (n1);
-    var num2 =Number (n2);
-    var suma = num1+num2;
-    if(isNaN(suma)){
-        return("No se puede realizar la suma");
-}
-else{ 
-    return("La suma es "+suma);
-    }
-}
+//Arreglos multidimensional:
+var matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
